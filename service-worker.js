f74ceaefc6afbc8f87a8aba4630be6f9435a799a
jan-martinek <jan-martinek@users.noreply.github.com>
2022-09-29 21:44:48 +0000

    Deploying to docs from @ books-are-next/stroupeznicky-nasi-furianti@fa56964551339aa4ae66aa7319280d0c184cd6bb 🚀

--- service-worker.js
+++ service-worker.js
@@ -764,7 +764,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 /// <reference lib="WebWorker" />
 require("regenerator-runtime");
 const sw = self;
-const CACHE = 'cache-ba68e8d';
+const CACHE = 'cache-fa56964';
 /**
  * IndexedDB singleton wrapper used to store persistent information with an predefined {@link Schema}
  */
